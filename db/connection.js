6f1a6d014aecbf0c83a2aeb9895c61c08c7fbc20
Farris <farris.ismati@gmail.com>
2018-01-11 23:19:52 -0500

    changes?

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mongoose    = require('mongoose')
 if (process.env.NODE_ENV == "production") {
   mongoose.connect(process.env.MLAB_URL)
 } else {
-  mongoose.connect("mongodb://localhost/code_god");
+  mongoose.connect("mongodb://localhost/scriptjs");
 }
 
 module.exports = mongoose
